b9382390fd3b9cec33f2d8fffa56801ec2d0e0ce
Your Name <you@example.com>
2026-05-05 10:25:55 +0530

    b

--- schemaValidation/projectsSchemaValidate.js
+++ schemaValidation/projectsSchemaValidate.js
@@ -10,11 +10,10 @@ export const projectsSchemaValidate = Joi.object({
     "any.required": "Project description is required"
   }),
   url_1: Joi.string().required().messages({
-    "string.empty": "Project description is required",
-    "any.required": "Project description is required"
+    "string.empty": "Project link is required",
+    "any.required": "Project link is required"
   }),
-  url_2: Joi.string().required().messages({
-    "string.empty": "Project description is required",
-    "any.required": "Project description is required"
+  url_2: Joi.string().allow("").optional().messages({
+    "string.base": "Optional project link must be text"
   }),
 });
